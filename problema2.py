d945fcd1e721dec5569bde9ee0c88c8fdcc16d9d
samuelbianch <samuelbianch38@gmail.com>
2023-03-27 15:05:43 -0300

    Implementando problema do triangulo de pascal em python

--- problema2.py
+++ problema2.py
@@ -1,24 +1,21 @@
-linhas_total = int(input("Quanntas linhas: "))
+linhas_total = int(input("Quantas linhas: "))
 
 linha_atual = 0
 coluna_atual = 0
-quant = 1
-quant_elementos_na_linha = 1
-i = 1
-n = 1
 fatorial_n = 1
 fatorial_p = 1
 fatorial_x = 1
 
 if linhas_total >= 1:
     while (linha_atual < linhas_total):
-        coluna_atual = 1
-        quant = linha_atual
-                
+        coluna_atual = 0     
         fatorial_n = 1
         fatorial_p = 1
         fatorial_x = 1
-        while (coluna_atual < quant + 1):
+        while (coluna_atual < linha_atual + 1):
+            fatorial_n = 1
+            fatorial_p = 1
+            fatorial_x = 1
             # n!
             n = linha_atual
             while (n > 0):
@@ -26,7 +23,7 @@ if linhas_total >= 1:
                 n = n - 1
             # p!
             p = coluna_atual
-            while (n > 0):
+            while (p > 0):
                 fatorial_p = fatorial_p * p
                 p = p - 1
             # (n - p)!
@@ -34,9 +31,9 @@ if linhas_total >= 1:
             while (aux > 0):
                 fatorial_x = fatorial_x * aux
                 aux = aux - 1
-            print("Fatorial n: ", fatorial_n)
-            print("Fatorial p: ", fatorial_p)
-            print("Fatorial x: ", fatorial_x)
+            # print("Fatorial n: ", linha_atual, " = ", fatorial_n)
+            # print("Fatorial p: ", coluna_atual, " = ", fatorial_p)
+            # print("Fatorial x: ", linha_atual - coluna_atual, " = ", fatorial_x)
             print(int((fatorial_n/(fatorial_p * fatorial_x))), end="")
             print(" ", end="")
             coluna_atual = coluna_atual + 1
