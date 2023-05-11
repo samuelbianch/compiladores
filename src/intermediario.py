3a8e4c680e16646515ef6566a0d6f8164d8a5c8a
samuelbianch <samuelbianch38@gmail.com>
2023-05-10 22:07:35 -0300

    Add: alocacao de memoria assembly

--- src/intermediario.py
+++ src/intermediario.py
@@ -15,7 +15,32 @@ class GeradorIntermediario():
         lista_aux.sort()
         self.lista_variaveis = lista_aux
     
+    def aloca_espaco_memoria(self):
+        string = ""
+
+        i = 0
+
+        while i < len(self.lista_variaveis):
+            string += self.lista_variaveis[i] + ": DB 8\n"
+            i += 1
+
+        return string
+    
+    def expressao_pos_fixa(self):
+        expressao = None
+
+
+        return expressao
+
+    def get_expressao_infixa(self):
+       return pass
+
     def gerador_intermediario(self):
         self.lista_variaveis = AnalisadorLexico.get_lista_variaveis_to_intermediario()
         self.remove_repetidos()
-        print(self.lista_variaveis)
+        
+        with open('../out/saida_lexico.txt', 'r') as arquivo:
+            lista = arquivo.readlines()
+        print(self.aloca_espaco_memoria())
+
+
