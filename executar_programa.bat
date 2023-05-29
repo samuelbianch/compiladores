bbc5b7c078f901f1d21c70a384edcd90fb5fc6f6
samuelbianch <samuelbianch38@gmail.com>
2023-05-29 16:29:14 -0300

    Add: obj e exe

--- executar_programa.bat
+++ executar_programa.bat
@@ -1,6 +1,6 @@
 C:
 cd "C:\Users\muca_\AppData\Local\bin\NASM"
-nasm -f elf "D:\Programação\compiladores\assembly\saida_assembly.asm"
-gcc -m32 -o -target "D:\Programação\compiladores\assembly\saida_assembly.o"
--target.exe
+nasm -fwin32 "D:\Programação\compiladores\assembly\saida_assembly.asm"
+gcc -m32 "D:\Programação\compiladores\assembly\saida_assembly.obj" -o "D:\Programação\compiladores\assembly\saida_assembly.exe"
+"D:\Programação\compiladores\assembly\saida_assembly.exe"
 pause
